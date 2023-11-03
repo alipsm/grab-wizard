1e6ba6c0b7e6f3705f731d7bf59d00d85cc022c3
alipsm <stooormix@gmail.com>
2023-11-03 19:24:01 +0330

    create fileOperations file for files management

--- lib/scripts/keyOperations.ts
+++ lib/scripts/keyOperations.ts
@@ -1,21 +1,33 @@
 import fakeJson from '../data/fakeJson.json'
+import { PATHS_JSON_FILE } from '../constant';
+import { createPathsJsonFile, updatePathsJsonFile } from './fileOperations';
+
+var fs = require('fs');
 
 // Creating a unique key using the object keys and the route received from the user
-function createUniqeKey(obj: object, path: string) {
-    // ایجاد یک استرینگ با فرمت URL query parameters
+function createUniqueKey(obj: object, path: string) {
     const getObjectKeys = Reflect.ownKeys(obj);
     const dataString = `objectKeys=${getObjectKeys.join(".")}&userPath=${path}`;
     return dataString
 }
 
-// ---------------------------------------------------------------------------------
 
 // Checking the presence of the key in the json file
-function isExistUniqeKey(key: string): boolean {
+function isExistUniqueKey(key: string): boolean {
     const innerFakeFile = fakeJson;
     if (key in innerFakeFile)
         return true
     return false
 }
 
-export { createUniqeKey, isExistUniqeKey }
+
+// Writes Unique key in the json file
+function setUniqueKey(key: string, path: string) {
+    if (fs.existsSync(PATHS_JSON_FILE)) {
+        updatePathsJsonFile(key, path)
+    } else {
+        createPathsJsonFile(key, path)
+    }
+}
+
+export { createUniqueKey, isExistUniqueKey, setUniqueKey }
