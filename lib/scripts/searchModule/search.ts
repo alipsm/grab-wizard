e2a25cac604a08705ccf291ce07440a0fd78d6df
alipsm <stooormix@gmail.com>
2023-11-18 23:45:20 +0330

    edite search structure and use pure function

--- lib/scripts/searchModule/search.ts
+++ lib/scripts/searchModule/search.ts
@@ -1,49 +1,56 @@
-interface navigatorInterface {
-    obj: object,
-    routes: string[],
-    options?: {
-        getPath?: boolean | undefined,
-    }
+import { NavigatorInterface } from "../../types"
+
+
+const isFinalItem = (obj: object, indexFound: number, key: string) => {
+    return Object.keys(obj).at(-1) === key && indexFound !== 0 && typeof obj[key] !== 'object'
+}
+
+const isObject = (obj: object, key: string) => {
+    return typeof obj[key] === 'object'
+}
+
+const isLastRoutesKey = (routes: (string | number)[], indexFound: number) => {
+    return indexFound === routes.length
+}
+
+const areSameKeys = (routes: (string | number)[], indexFound: number, key: string) => {
+    return key === routes[indexFound]
 }
 
 
-// Callback function for find nested value
-function findValueByNavigateKey(navData: navigatorInterface) {
 
-    // Local states for inner function
+
+// Callback function for find nested value
+function findValueByNavigateKey(navData: NavigatorInterface) {
+
+    const { obj, routes, options } = navData;
+
     var indexFound = 0; // => Controls the index number of found routes
     var routesMonitoring = [] // => Monitores object routes 
 
-    // handle callback search function (find nested value)
-    function search(searchNavData: navigatorInterface) {
-        for (const key in searchNavData.obj) {
-            if (key === searchNavData.routes[indexFound]) {
+    // handle callback search function (find nested value & path)
+    function search(obj: object, routes: (string | number)[], options: object) {
+        for (const key in obj) {
+            if (areSameKeys(routes, indexFound, key)) {
                 routesMonitoring.push(key)
                 indexFound++
-                if (indexFound === searchNavData.routes.length) {
-                    return view(searchNavData.obj[key])
-                } else {
-                    searchNavData.obj = searchNavData.obj[key];
-                    const result = search({ obj: searchNavData.obj, routes: searchNavData.routes, options: searchNavData.options });
-                    if (result !== undefined) {
-                        return result;
-                    }
-                    return undefined
-                }
-            } else if (Object.keys(searchNavData.obj).at(-1) === key && indexFound !== 0 && typeof searchNavData.obj[key] !== 'object') {
+                if (isLastRoutesKey(routes, indexFound))
+                    return view(obj[key])
+                const nextObj = obj[key];
+                const result = search(nextObj, routes, options);
+                return result ?? undefined
+            } else if (isFinalItem(obj, indexFound, key)) {
                 return undefined
-            } else if (typeof searchNavData.obj[key] === 'object') {
+            } else if (isObject(obj, key)) {
                 routesMonitoring.push(key)
-                const result = search({ obj: searchNavData.obj[key], routes: searchNavData.routes, options: searchNavData.options });
-                if (result !== undefined) {
+                const result = search(obj[key], routes, options);
+                if (result)
                     return result;
-                }
             }
         }
         routesMonitoring.pop()
         return undefined;
     }
-
 
     // Manage output data settings
     function view(data: unknown) {
@@ -52,11 +59,7 @@ function findValueByNavigateKey(navData: navigatorInterface) {
         }
         return data
     }
-
-
-    return search(navData)
+    return search(obj, routes, options)
 }
 
-export {
-    findValueByNavigateKey
-}
+export { findValueByNavigateKey }
