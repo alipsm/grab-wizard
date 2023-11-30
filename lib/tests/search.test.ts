ca727679cc30a513d644a63e55c66adfe088079a
alipsm <stooormix@gmail.com>
2023-12-01 01:04:45 +0330

    set new test for get default value

--- lib/tests/search.test.ts
+++ lib/tests/search.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect, beforeAll } from "@jest/globals";
 import FakeApiData from "./sample/index.json"
-const { grabPath, grabValue } = require("../scripts/searchModule/search")
+import DiContainer from "../scripts/di/container";
+const { grabPath, grabValue } = DiContainer
 
 
 describe('search module', () => {
@@ -12,47 +13,60 @@ describe('search module', () => {
     });
 
     test('find nested value for object', () => {
-        expect(grabValue(fakeData, ["taglib", "nested2"])).toMatchObject({ value: 'hi' });
+        expect(grabValue(fakeData, "taglib.nested2")).toMatchObject({ value: 'hi' });
     });
 
     test('find one before the last item value in array', () => {
-        expect(grabValue(fakeData, ["last-array-item", "adminGroupID"])).toBe(4);
+        expect(grabValue(fakeData, "last-array-item.adminGroupID")).toBe(4);
     });
 
     test('find last item value in array', () => {
-        expect(grabValue(FakeApiData, ["last-array-item", "betaServer"])).toBe(true);
+        expect(grabValue(FakeApiData, "last-array-item.betaServer")).toBe(true);
     });
 
     test('find last item value in array', () => {
-        expect(grabValue(FakeApiData, ["last-array-item", "betaServer"])).toBe(true);
+        expect(grabValue(FakeApiData, "last-array-item.betaServer")).toBe(true);
     });
 
     test('get navigate path in object', () => {
-        expect(grabPath(FakeApiData, ["taglib", "value"]
+        expect(grabPath(FakeApiData, "taglib.value"
         )).toBe(".web-app.taglib.nested1.nested2.value");
     });
 
 
     test('get navigate path in array', () => {
-        expect(grabPath(FakeApiData, ["init-param", "mailHostOverride"]
-        )).toBe(".web-app.servlet[2].init-param.mailHostOverride");
+        expect(grabPath(FakeApiData, "init-param.mailHost"
+        )).toBe(".web-app.servlet[2].init-param.mailHost");
     });
 
 
 
     // Test modes of incorrect routes
     test('select side key (wrong test)', () => {
-        expect(grabPath(FakeApiData, ["servlet-mapping", "taglib"]
-        )).toBe("");
+        expect(grabPath(FakeApiData, "servlet-mapping.taglib"
+        )).toBe(undefined);
     });
 
     test('apply nested wrong navigate path in array (wrong test)', () => {
-        expect(grabPath(FakeApiData, ["servlet-mapping", "nested1"]
-        )).toBe("");
+        expect(grabPath(FakeApiData, "servlet-mapping.nested1"
+        )).toBe(undefined);
     });
 
     test('get navigate path in array (wrong test)', () => {
-        expect(grabPath(FakeApiData, ["init-param", "wrong"]
-        )).toBe("");
+        expect(grabPath(FakeApiData, "init-param.wrong"
+        )).toBe(undefined);
+    });
+
+
+    let defaultValue = "my default string for wrong operation"
+    // Test default value
+    test('get default value with set wrong path for grabPath', () => {
+        expect(grabPath(FakeApiData, "wrong-path", defaultValue
+        )).toBe(defaultValue);
+    });
+
+    test('get default value with set wrong path for grabValue', () => {
+        expect(grabValue(FakeApiData, "wrong-path", defaultValue
+        )).toBe(defaultValue);
     });
 });
