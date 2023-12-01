1a9cefb221f3c7a7e574d2ef14e17e99d3040c2d
alipsm <stooormix@gmail.com>
2023-12-01 21:06:10 +0330

    edit grabPath type

--- lib/scripts/search-operation/module/index.ts
+++ lib/scripts/search-operation/module/index.ts
@@ -3,11 +3,11 @@ import navigateAndRetrieveHelpers from "../helpers";
 
 const { areSameKeys, isFinalItem, isLastRoutesKey, isObject } = navigateAndRetrieveHelpers;
 const retrieveNavigator = (() => {
-    const grabValue = (obj: object,routes: NavigatorInterface["routes"],defaultValue?:string) => {
+    const grabValue = (obj: object, routes: NavigatorInterface["routes"], defaultValue?: string) => {
         const result = navigateAndRetrieve({ obj, routes, defaultValue });
         return result;
     };
-    const grabPath = (obj: object,routes: NavigatorInterface["routes"],defaultValue?:string) => {
+    const grabPath = (obj: object, routes: NavigatorInterface["routes"], defaultValue?: string):NavigatorInterface["routes"] => {
         const { grabPath } = navigateAndRetrieve({ obj, routes, defaultValue });
         return grabPath;
     };
@@ -17,12 +17,12 @@ const retrieveNavigator = (() => {
 })();
 export default retrieveNavigator;
 // Callback function for find nested value
-function navigateAndRetrieve(navData:NavigatorInterface) {
+function navigateAndRetrieve(navData: NavigatorInterface):{grabPath:NavigatorInterface["routes"],grabValue:any} {
     const { obj, routes } = navData;
     var indexFound = 0; // => Controls the index number of found routes
     var routesMonitoring = []; // => Monitores object routes 
     // handle callback search function (find nested value & path)
-    function search(obj:object, routes:NavigatorInterface["routes"]) {
+    function search(obj: object, routes: NavigatorInterface["routes"]) {
         for (const key in obj) {
             const nextObj = obj[key];
             if (areSameKeys(routes, indexFound, key)) {
@@ -34,14 +34,15 @@ function navigateAndRetrieve(navData:NavigatorInterface) {
                 const result = search(nextObj, routes);
                 return result;
             }
-            else if (isFinalItem(obj, indexFound, key)) {
-                return undefined;
-            }
             else if (isObject(obj, key)) {
                 routesMonitoring.push(key);
                 const result = search(nextObj, routes);
                 if (result)
                     return result;
+            }
+            else if (isFinalItem(obj, indexFound, key)) {
+                routesMonitoring.pop();
+                return undefined;
             }
         }
         routesMonitoring.pop();
@@ -49,7 +50,7 @@ function navigateAndRetrieve(navData:NavigatorInterface) {
     }
     const result = search(obj, routes);
     return {
-        grabValue:  result?.value ?? navData.defaultValue,
-        grabPath:  result?.path ?? []
+        grabValue: result?.value ?? navData.defaultValue,
+        grabPath: result?.path ?? []
     };
 }
