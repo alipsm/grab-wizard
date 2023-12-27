78fcf939e2ff81b024d8ae9669dc192c02e7563a
alipsm <stooormix@gmail.com>
2023-12-27 12:28:14 +0330

    remove all symbol in object keys

--- lib/scripts/file-operation/helpers/index.ts
+++ lib/scripts/file-operation/helpers/index.ts
@@ -9,20 +9,23 @@ const fileHelper = (() => {
         return false;
     }
     // Checking the presence of the key in the json file
-    function isExistWindow():"browser"|"node" {
+    function isExistWindow(): "browser" | "node" {
         if (typeof window === "object")
             return "browser";
         return "node";
     }
 
-    function isExistPath(path:string):boolean {
+    function isExistPath(path: string): boolean {
         return !!fs.existsSync(path)
     }
 
+    const _removeSymbolKeys = (objKeys: any[]) => (objKeys.filter((objKey: any) => (typeof objKey !== "symbol")))
+
     // Creating a unique key using the object keys and the route received from the user
-    function createUniqueKey(obj:object, path:string) {
-        const getObjectKeys = Reflect.ownKeys(obj);
-        let dataString:string = `${getObjectKeys.join(".")}&${path}`;
+    function createUniqueKey(obj: object, path: string) {
+        const getObjectKeys = Reflect.ownKeys(obj)
+        const filterBySymbol = _removeSymbolKeys(getObjectKeys)
+        let dataString: string = `${filterBySymbol.join(".")}&${path}`;
         return dataString;
     }
 
